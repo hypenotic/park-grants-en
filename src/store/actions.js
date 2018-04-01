c182ad63b90561c303b8c5544df716686bd54b83
Emily Dela Cruz <delacruz.emily@gmail.com>
2018-04-01 15:27:40 -0400

    Updates endpoints and scripts

--- src/store/actions.js
+++ src/store/actions.js
@@ -6,8 +6,8 @@ export const actions = {
     getLocations({commit, dispatch, context, state}, info) {
         console.log('getLocations dispatched');
         // axios.get('http://dev.hypenotic.com/pp-map/wp-json/mapdata/v1/all')
-        // axios.get('https://parkpeople.ca/listings/wp-json/mapdata/v1/all')
-        axios.get('http://dev.hypenotic.com/pp-map/wp-json/eventdata/v1/all')
+        axios.get('https://parkpeople.ca/listings/wp-json/mapdata/v1/all')
+        // axios.get('http://dev.hypenotic.com/pp-map/wp-json/eventdata/v1/all')
         .then(function (response) {
             // let pageSlugKeys = _.mapKeys(response.data, 'slug');
             // commit(types.SET_PAGE_LIST, response.data);
@@ -21,8 +21,8 @@ export const actions = {
     getActivities({commit, dispatch, context, state}, info) {
         console.log('getActivities dispatched');
         // axios.get('http://dev.hypenotic.com/pp-map/wp-json/mapdata/v1/all')
-        // axios.get('https://parkpeople.ca/listings/wp-json/mapdata/v1/all')
-        axios.get('http://dev.hypenotic.com/pp-map/wp-json/activitydata/v1/all')
+        axios.get('https://parkpeople.ca/listings/wp-json/mapdata/v1/all')
+        // axios.get('http://dev.hypenotic.com/pp-map/wp-json/activitydata/v1/all')
         .then(function (response) {
             commit(types.SET_ACTIVITY_LIST, response.data);
         })
