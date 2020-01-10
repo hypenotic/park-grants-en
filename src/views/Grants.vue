324a37361a0389bea7e5ed602ad4ad86b3f54630
Hubert <hubhcsi@gmail.com>
2020-01-10 11:20:47 -0500

    change video src

--- src/views/Grants.vue
+++ src/views/Grants.vue
@@ -2,7 +2,7 @@
 	<div v-if="data != null">
 		<section class="videos">
 			<div class="hero">
-				<iframe src="https://player.vimeo.com/video/374961083?autoplay=1&loop=1" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+				<iframe src="https://player.vimeo.com/video/374961083?background=1&loop=1" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
 			</div>
 		</section>
 		<section class="section" v-if="data && data.hasOwnProperty('meta_box')">
