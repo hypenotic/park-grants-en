cd8d6d2f755e5bb77a46163432f74fbb14928ea1
Hubert <hubhcsi@gmail.com>
2020-01-13 11:34:13 -0500

    change nav

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -18,8 +18,9 @@
         <div id="navbarExampleTransparentExample" class="navbar-menu" v-bind:class="{ 'menu-open': showMobileMenu }">
             <div class="navbar-end">
                 <a href="https://parkpeople.ca" class="navbar-item">Home</a>
-                <span class="navbar-item" v-on:click="showMobileMenu = !showMobileMenu" v-if="this.$route.path == '/grant-recipients'"><router-link to="/" exact>Back to Grants Page</router-link></span>
-                <span class="navbar-item" v-on:click="showMobileMenu = !showMobileMenu" v-if="this.$route.path == '/'"><router-link to="/grant-recipients"  exact>2019 Recipients</router-link></span>
+                <span class="navbar-item" v-on:click="showMobileMenu = !showMobileMenu" v-if="this.$route.path != '/'"><router-link to="/" exact>Back to Grants Page</router-link></span>
+                <span class="navbar-item" v-on:click="showMobileMenu = !showMobileMenu" v-if="this.$route.path != '/faq'"><router-link to="/faq" exact>FAQ</router-link></span>
+                <span class="navbar-item" v-on:click="showMobileMenu = !showMobileMenu" v-if="this.$route.path != '/grant-recipients'"><router-link to="/grant-recipients"  exact>2019 Recipients</router-link></span>
             </div>
         </div>
     </nav>
@@ -42,8 +43,9 @@
         <div id="navbarExampleTransparentExample" class="navbar-menu" v-bind:class="{ 'menu-open': showMobileMenu }">
             <div class="navbar-end">
                 <a href="https://parkpeople.ca" class="navbar-item">Home</a>
-                <span class="navbar-item" v-on:click="showMobileMenu = !showMobileMenu" v-if="this.$route.path == '/grant-recipients'"><router-link to="/" exact>Back to Grants Page</router-link></span>
-                <span class="navbar-item" v-on:click="showMobileMenu = !showMobileMenu" v-if="this.$route.path == '/'"><router-link to="/grant-recipients" exact>2019 Recipients</router-link></span>
+                <span class="navbar-item" v-on:click="showMobileMenu = !showMobileMenu" v-if="this.$route.path != '/'"><router-link to="/" exact>Back to Grants Page</router-link></span>
+                <span class="navbar-item" v-on:click="showMobileMenu = !showMobileMenu" v-if="this.$route.path != '/faq'"><router-link to="/faq" exact>FAQ</router-link></span>
+                <span class="navbar-item" v-on:click="showMobileMenu = !showMobileMenu" v-if="this.$route.path != '/grant-recipients'"><router-link to="/grant-recipients"  exact>2019 Recipients</router-link></span>
             </div>
         </div>
     </nav>
