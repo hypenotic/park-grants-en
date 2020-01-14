167efa3b7ad45435f8b274333050470a1d6f97f7
Hubert <hubhcsi@gmail.com>
2020-01-14 10:39:13 -0500

    try this

--- src/views/Grants.vue
+++ src/views/Grants.vue
@@ -9,7 +9,7 @@
 					Learn more
 				</a>
 			</div>
-			<div class="hero" v-if="isMobile">
+			<div class="hero" v-if="isMobile()">
 				<iframe v-if="selectedVideo == 0" src="https://player.vimeo.com/video/374742599?background=1&loop=1&autoplay=0" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
 				<iframe v-if="selectedVideo == 1" src="https://player.vimeo.com/video/374961083?background=1&loop=1&autoplay=0" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
 				<iframe v-if="selectedVideo == 2" src="https://player.vimeo.com/video/374963755?background=1&loop=1&autoplay=0" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
